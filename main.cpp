f285a5d314a4bfd6ac82ab064800ced032917aea
annnsshhhu <anshu8409545126@gmail.com>
2025-06-14 23:22:37 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+#include <cstdlib>
+#include <ctime>
+#include<vector>
+using namespace std;
+void drawBoard(vector<vector<char>>& board){
+    for(int i=0;i<board.size();i++){
+        for(int j=0;j<board[i].size();j++){
+             cout<<board[i][j];
+            if(j<2){
+           cout<<"|";
+            }
+        }
+        if(i < board.size()-1){
+        cout<<endl<<"-----"<<endl;
+        }
+    }
+    cout<<endl;
+    return;
+}
+
+bool check(vector<vector<char>>& board,char ch){
+    int clarity1,clarity2;
+    for(int i=0;i<3;i++){
+        
+        clarity1 = 0,clarity2 = 0;
+        for(int j=0;j<3;j++){
+            if(board[i][j] == ch){
+                clarity1++;
+            }
+            if(board[j][i] == ch){
+                clarity2++;
+            }
+        }
+        if(clarity1 == 3 || clarity2 == 3){
+            if(ch == 'X'){cout<<"You Won "<<endl;
+            return true;
+            }
+            else{
+                cout<<"You lost "<<endl;
+            return true;
+            }
+        }
+    }
+    clarity1 = 0,clarity2 = 0;
+    for(int i=0,j=0;i<3,j<3;i++,j++){
+        if(board[i][j] == ch){
+            clarity1++;
+        }
+        else{
+            break;
+        }
+    }
+    for(int i=0,j=2;i<3,j>=0;i++,j--){
+        if(board[i][j] == ch){
+            clarity2++;
+        }
+        else{
+            break;
+        }
+    }
+    if(clarity1 == 3 || clarity2 == 3){
+            if(ch == 'X'){cout<<"You Won "<<endl;
+            return true;
+            }
+            else{
+                cout<<"You lost "<<endl;
+            return true;
+            }
+        }
+        return false;
+}
+
+int main(){
+    vector<vector<char>>board(3,vector<char>(3,' '));
+    srand(time(0)); 
+    cout<<"Welcome to TIC-TAC-TOE Game : "<<endl<<endl;
+    drawBoard(board);
+    for(int k=0; ;k++){
+        int attempt = 0;
+        int iX,jX;
+        int iO,jO;
+        for(int m=0; ;m++){
+    cout<<"PlayerX , Enter the position (0-3) for row and column : ";
+    cin>>iX>>jX;
+    if(board[iX][jX] == ' '){
+        board[iX][jX] = 'X';
+        break;
+    }
+        }
+    cout<<endl;
+    drawBoard(board);
+    cout<<endl;
+    if(check(board,'X')){
+        break;
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(board[i][j] != ' '){
+                attempt++;
+            }
+        }
+    }
+    
+    cout<<endl<<"Total no. of non-empty seats "<<attempt<<endl;
+    if(attempt == 9){
+        cout<<"Tie"<<endl;
+        break;
+        
+    }
+    for(int m=0; ;m++){
+    iO = rand() % 3;
+    jO = rand() % 3;
+    if(board[iO][jO] == ' '){
+        board[iO][jO] = 'O';
+        break;
+    }
+    }
+    cout<<endl;
+    drawBoard(board);
+    cout<<endl;
+    if(check(board,'O')){
+        break;
+    }
+    }
+}
